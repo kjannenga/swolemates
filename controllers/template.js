9166d0e72c10f1bcb032be8e63a6d556446e295a
Krystine Jannenga <kjannenga1@gmail.com>
2019-06-20 08:53:57 -0700

    home button when logged in on index

--- controllers/template.js
+++ controllers/template.js
@@ -3,7 +3,7 @@ const knex = require("../db/knex.js");
 module.exports = {
   // CHANGE ME TO AN ACTUAL FUNCTION
   index: function(req, res) {
-    res.render('index');
+    res.render('index', {user:req.session.user});
   },
   loginPage: function(req, res) {
     res.render('login');
